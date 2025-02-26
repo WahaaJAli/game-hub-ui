4b4fc47be9183fb1abe274e32d93af6e029a1e66
WahaaJAli <73385761+WahaaJAli@users.noreply.github.com>
2025-02-26 17:16:07 +0500

    Icons Refactor

--- src/icons/Icons.ts
+++ src/icons/Icons.ts
@@ -1,27 +1,26 @@
 import { BsBank as Bank } from "react-icons/bs";
+import { BsGlobe as Globe } from 'react-icons/bs'
+import { FaAndroid as Android, FaApple as Apple, FaLinux as Linux, FaPlaystation as Playstation, FaWindows as Windows, FaXbox as Xbox } from 'react-icons/fa'
 import { GiButterfly as Butterfly, GiButterflyFlower as ButterflyFlower } from "react-icons/gi"
-import { PiButterflyLight as ButterflyLight } from "react-icons/pi"
-
-import { FaWindows, FaPlaystation, FaXbox, FaAndroid, FaLinux, FaApple } from 'react-icons/fa'
-import { MdPhoneIphone } from 'react-icons/md'
-import { SiNintendo } from 'react-icons/si'
-import { BsGlobe } from 'react-icons/bs'
 import { IconType } from "react-icons/lib";
+import { MdPhoneIphone as Iphone } from 'react-icons/md'
+import { PiButterflyLight as ButterflyLight } from "react-icons/pi"
+import { SiNintendo as Nintendo } from 'react-icons/si'
 
 const Icons: { [key: string]: IconType } = {
+  Android,
+  Apple,
   Bank,
-  Globe: BsGlobe,
   Butterfly,
   ButterflyFlower,
   ButterflyLight,
-  Android: FaAndroid,
-  Apple: FaApple,
-  Linux: FaLinux,
-  Playstation: FaPlaystation,
-  Windows: FaWindows,
-  Xbox: FaXbox,
-  Iphone: MdPhoneIphone,
-  Nintendo: SiNintendo,
+  Globe,
+  Iphone,
+  Linux,
+  Nintendo,
+  Playstation,
+  Windows,
+  Xbox,
 }
 
 export default Icons
