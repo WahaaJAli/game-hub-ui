02851823eb1f557db3c8e5736c271868f38977ca
WahaaJAli <73385761+WahaaJAli@users.noreply.github.com>
2025-04-18 14:46:50 +0500

    App Logo & Icon Add

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,16 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Image, useColorModeValue } from '@chakra-ui/react'
 import { JSX } from 'react'
 import ColorModeSwitch from './ColorModeSwitch'
 import logo from '../svgs/aveda.svg'
 import SearchInput from './SearchInput'
 
 const NavBar = (): JSX.Element => {
+  const filter = useColorModeValue('invert(0)', 'invert(1)')
+
   return (
     <>
       <HStack py={6}>
-        <Image src={logo} width='10%' mr='10px' />
+        <Image src={logo} width='10%' mr='10px' filter={filter} />
         {/* <Text whiteSpace='nowrap' fontSize='xl'>AQSH GAME HUB</Text> */}
         <SearchInput />
         <ColorModeSwitch />
